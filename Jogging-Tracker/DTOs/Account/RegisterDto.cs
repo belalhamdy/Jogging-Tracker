d82d2a8b109e5b43ccf6dfa950956471aa0fd380
Belal Hamdy <belalhezzat@gmail.com>
2022-01-30 10:44:44 +0200

    Added `RegisterDto` data

--- Jogging-Tracker/DTOs/Account/RegisterDto.cs
+++ Jogging-Tracker/DTOs/Account/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,17 @@ namespace Jogging_Tracker.DTOs.Account
 {
     public class RegisterDto
     {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        [Required(ErrorMessage = "Email is required")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
     }
 }
