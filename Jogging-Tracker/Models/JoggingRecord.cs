d4a4a246597010bf70bd10468be1901c2f5157c8
Belal Hamdy <belalhezzat@gmail.com>
2022-01-30 11:04:29 +0200

    Added init `JoggingRecord`.

---
+++ Jogging-Tracker/Models/JoggingRecord.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jogging_Tracker.Models
+{
+    public class JoggingRecord
+    {
+    }
+}
