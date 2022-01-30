a079b83297d3d3b5bbb7117875df23c2f731c2fd
Belal Hamdy <belalhezzat@gmail.com>
2022-01-30 10:45:11 +0200

    Added `LoginDto` data.

--- Jogging-Tracker/DTOs/Account/LoginDto.cs
+++ Jogging-Tracker/DTOs/Account/LoginDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace Jogging_Tracker.DTOs.Account
 {
     public class LoginDto
     {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
     }
 }
