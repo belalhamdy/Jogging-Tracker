b54d8fe16bf0471fdf6db5c56eb5b47632a276c6
Belal Hamdy <belalhezzat@gmail.com>
2022-01-30 10:43:04 +0200

    Initialize Register and Login DTOs (Data Transfer Objects).

---
+++ Jogging-Tracker/DTOs/Account/RegisterDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jogging_Tracker.DTOs.Account
+{
+    public class RegisterDto
+    {
+    }
+}
